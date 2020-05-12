3a2e63cab52187c39385b074eef555f0961e6fcb
klittle314 <klittle@iecodesign.com>
2020-05-12 14:45:34 -0500

    change death to event in help text; change instructions on the upload tab for events

--- ui.R
+++ ui.R
@@ -11,9 +11,9 @@ shinyUI(navbarPage("COVID-19 Control Chart Application",
                             wellPanel(
                                 tags$style(type="text/css", '#leftPanel { width:200px; float:left;}'),
                                 
-                                tags$blockquote("This project implements a method based on control charts to view phases in daily reported deaths from COVID-19.
+                                tags$blockquote("This project implements a method based on control charts to view phases in daily reported events from COVID-19.
                                 The method was developed by Lloyd Provost, Shannon Provost, Rocco Perla, Gareth Parry, and Kevin Little. The code is R and deploys a user interface using Shiny technology.
-                                The R code transforms a time series of daily reported deaths into charts that distinguish phases of COVID-19 infection for a reporting location like a country, state or city"),
+                                The R code transforms a time series of daily reported events into charts that distinguish phases of COVID-19 infection for a reporting location like a country, state or city"),
                                 tags$hr(),
                                 
                                 tags$p(tags$img(src='example_annotated.jpg', align = "top")),
@@ -75,9 +75,14 @@ shinyUI(navbarPage("COVID-19 Control Chart Application",
                                     span("Created by "),
                                     a("Kevin Little", href = "mailto:klittle@iecodesign.com"),
 
-                                    span("updated 8 May 2020 7:00am U.S. CDT"),
+
+
+                                    span("updated 12 May 2020 12:15pm U.S. CDT"),
+
 
                                     br(), br()
+                                    
+                                
                                 
                                 )
                                 
@@ -91,18 +96,19 @@ shinyUI(navbarPage("COVID-19 Control Chart Application",
                      
                      tags$ul(
                        tags$li('date (MM/DD/YYYY format)'),
-                       tags$li('cases'),
-                       tags$li('deaths'),
-                       tags$li('location'),
+                       tags$li('location')),
+                     
+                     h4('Then add as many numeric columns as you wish. Include column names in the first row, and use underscores (_) to separate words in names (e.g. "icu_patients").'),
+                     
                      br(),
                      helpText('You may include multiple locations in a single file; the locations will appear in the location drop-down box'),
+
                      
-                     helpText('The file should contain new daily reported daily deaths (not cumulative).  We will generalize to allow other events soon.'),
+                     helpText('The file should contain new daily reported events (not cumulative).'),
                      
-                     helpText('The current code will not yet handle NA values in the death series.  Zero values are fine.')
-                     ),
-                     
-                     
+                     helpText('The current code will not yet handle NA values in the death series.  Zero values are fine.'),
+                    
+
                      h5('Click',
                         tags$a('here', 
                                href = 'https://support.office.com/en-us/article/Import-or-export-text-txt-or-csv-files-5250ac4c-663c-47ce-937b-339e391393ba',
@@ -143,6 +149,12 @@ shinyUI(navbarPage("COVID-19 Control Chart Application",
                                       label   = h5('Choose data source'),
                                       choices = data_choices,
                                       selected = data_selected),
+                                    
+                                    selectInput(
+                                      inputId = 'event_name',
+                                      label   = h5('Choose event column'),
+                                      choices = NULL,
+                                      selected = NULL),
                                     
                                     #drop down to select the Site Type
                                     # htmlOutput("selectSiteType"),
@@ -189,8 +201,8 @@ shinyUI(navbarPage("COVID-19 Control Chart Application",
                                         value   = TRUE),
                                       
                                       #Input date that marks the start of the limit calculations
-                                      dateInput("start_date",label=h5("Custom start date for calculations instead of date of first death"),value=defStartdate),
-                                      helpText(h6("Leave blank to allow the start date to be determined as date of first reported death")),
+                                      dateInput("start_date",label=h5("Custom start date for calculations instead of date of first event"),value=defStartdate),
+                                      helpText(h6("Leave blank to allow the start date to be determined as date of first reported event")),
                                       #helpText(h6("The starting date 2019-12-31 tells the app to use all the available data.")),
                                       helpText(h6("You can choose a date after start of the series to focus the graph and calculations on a shorter date range.")),
                                      
